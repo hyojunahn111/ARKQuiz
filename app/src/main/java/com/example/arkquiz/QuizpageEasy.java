aab36996cb18d70984c11a048e79262c9e022350
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2020-07-03 06:28:15 +0900

    문제 추가
    문제 밑부분에 주석으로 달아논 문제 있는데 이런 느낌은 어떰

--- app/src/main/java/com/example/arkquiz/QuizpageEasy.java
+++ app/src/main/java/com/example/arkquiz/QuizpageEasy.java
@@ -14,6 +14,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -64,6 +65,7 @@ public class QuizpageEasy extends AppCompatActivity {
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
+
         mAdView = findViewById(R.id.adView_quiz_easy);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
