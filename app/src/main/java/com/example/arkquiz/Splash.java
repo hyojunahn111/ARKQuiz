1ea6f64f5988fd0abcaf45bbf55d63ba41a4896c
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2020-07-06 00:04:48 +0900

    스플래쉬 화면, 아이콘 추가

---
+++ app/src/main/java/com/example/arkquiz/Splash.java
@@ -0,0 +1,32 @@
+package com.example.arkquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 1000);
+
+    }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            Splash.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+}
