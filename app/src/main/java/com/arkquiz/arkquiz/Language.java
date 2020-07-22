cac261603d5010b03fca818ad2e5c5f721b2c353
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2020-07-23 05:46:33 +0900

    Kor -> Eng 50%

---
+++ app/src/main/java/com/arkquiz/arkquiz/Language.java
@@ -0,0 +1,66 @@
+package com.arkquiz.arkquiz;
+
+import android.app.AppComponentFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Language extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TranslateTask translateTask = new TranslateTask();
+        translateTask.execute();
+    }
+
+    class TranslateTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String clientId = "wwEOQvan8Qd8Sv6r0__T";//애플리케이션 클라이언트 아이디값";
+            String clientSecret = "HqMcU9SiUQ";//애플리케이션 클라이언트 시크릿값";
+            try {
+                String text = URLEncoder.encode("", "UTF-8");
+                String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("X-Naver-Client-Id", clientId);
+                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                // post request
+                String postParams = "source=ko&target=en&text=" + text;
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(postParams);
+                wr.flush();
+                wr.close();
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if(responseCode==200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            return null;
+        }
+    }
+
+}
